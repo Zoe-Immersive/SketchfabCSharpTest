90f88b0b46ff6e70773be24e551e956d73122205
4nc3str4l <muriel.maths@gmail.com>
2022-01-17 18:24:27 +0100

    Testing Project

---
+++ Sketchfab Wrapper/Assets/Tester/Misc/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float Speed;
+    public float SpeedH = 2.0f;
+    public float SpeedV = 2.0f;
+
+    private float m_Yaw = 0.0f;
+    private float m_Pich = 0.0f;
+
+    private Camera m_Camera;
+
+    private void Awake()
+    {
+        m_Camera = GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Speed * transform.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Speed * transform.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Speed * transform.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Speed * transform.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Speed * transform.up * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            transform.position -= Speed * transform.up * Time.deltaTime;
+        }
+
+        //m_Pich -= SpeedV * Input.GetAxis("Mouse X");
+        //m_Camera.transform.localEulerAngles = new Vector3(45f, m_Pich, 0.0f);
+    }
+
+    public void PlaceObjectInFront(GameObject _object)
+    {
+
+        _object.transform.position = transform.position + m_Camera.transform.forward * 1;
+    }
+}
---
+++ Sketchfab Wrapper/Assets/Tester/TestApi.cs
@@ -0,0 +1,154 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestApi : MonoBehaviour
+{
+    private string m_Email = "your@email.com";
+    private string m_Password = "pass";
+    private string m_AccessToken = string.Empty;
+
+    private string m_UserInformation = string.Empty;
+    private List<SketchfabModel> m_ModelList = new List<SketchfabModel>();
+    private CameraController m_CameraController;
+
+    private void Start()
+    {
+        m_CameraController = GameObject.FindObjectOfType<CameraController>();        
+    }
+
+    public void Login()
+    {
+        SketchfabAPI.GetAccessToken(m_Email, m_Password, (SketchfabResponse<SketchfabAccessToken> answer) =>
+        {
+            if(answer.Success)
+            {
+                m_AccessToken = answer.Object.AccessToken;
+                SketchfabAPI.AuthorizeWithAccessToken(answer.Object);
+                Debug.Log(answer.Object.ToString());
+
+                GetUserInformation();
+                GetModelList();
+            }
+            else
+            {
+                Debug.LogError(answer.ErrorMessage);
+            }
+
+        });
+    }
+
+    public void GetUserInformation()
+    {
+
+        SketchfabAPI.GetUserInformation((SketchfabResponse<SketchfabUserInfo> answer) => {
+            if (answer.Success)
+            {
+                m_UserInformation = answer.Object.ToString();
+            }
+            else
+            {
+                Debug.Log(answer.ErrorType);
+                Debug.Log(answer.ErrorMessage);
+            }
+        });
+    }
+
+    public void GetModelList()
+    {
+        //UnityWebRequestSketchfabModelList.Parameters p = new UnityWebRequestSketchfabModelList.Parameters();
+        //p.downloadable = true;
+        //SketchfabAPI.GetModelList(p,((SketchfabResponse<SketchfabModelList> _answer) =>
+        //{
+        //    SketchfabResponse<SketchfabModelList> ans = _answer;
+        //    m_ModelList = ans.Object.Models;
+        //}));
+        UnityWebRequestSketchfabModelList.Parameters p = new UnityWebRequestSketchfabModelList.Parameters();
+        p.downloadable = true;
+        string searchKeyword = "Cat";
+        SketchfabAPI.ModelSearch(((SketchfabResponse<SketchfabModelList> _answer) =>
+        {
+            SketchfabResponse<SketchfabModelList> ans = _answer;
+            m_ModelList = ans.Object.Models;
+        }), p, searchKeyword, "white");
+    }
+
+    private void OnGUI()
+    {
+
+        if (!SketchfabAPI.Authorized)
+        {
+            GUI.color = Color.white;
+            m_Email = GUI.TextField(new Rect(10, 10, 200, 20), m_Email);
+            m_Password = GUI.PasswordField(new Rect(10, 30, 200, 20), m_Password, '*');
+
+            if (GUI.Button(new Rect(10, 50, 150, 20), "Login"))
+            {
+                Login();
+            }
+        }
+        else
+        {
+            GUI.color = Color.black;
+            RenderUserInfo();
+            if (GUI.Button(new Rect(10, 130, 250, 20), "Logout"))
+            {
+                SketchfabAPI.Logout();
+            }
+            RenderModelList();
+        }
+
+    }
+
+    private void RenderUserInfo()
+    {
+        if(this.m_UserInformation.Length == 0)
+        {
+            return;
+        }
+        GUI.Label(new Rect(10, 10, 150, 20), "User Information:");
+        GUI.Label(new Rect(10, 30, 250, 100), m_UserInformation);
+    }
+
+    private void RenderModelList()
+    {
+        if (m_ModelList.Count == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < m_ModelList.Count; ++i)
+        {
+            SketchfabModel m = m_ModelList[i];
+            if (m.IsDownloadable)
+            {
+                RenderModelListElement(m_ModelList[i], i);
+            }
+        }
+    }
+
+    private void RenderModelListElement(SketchfabModel _model, int y)
+    {
+        GUI.color = Color.black;
+        GUI.Label(new Rect(Screen.width - 250, y * 20 + 20, 120, 30), _model.Name);
+        GUI.color = Color.white;
+        if (GUI.Button(new Rect(Screen.width - 250 + 120, y * 30 + 20, 100, 20), "Download"))
+        {
+            DownloadModel(_model.Uid);
+        }
+    }
+
+    private void DownloadModel(string _guid)
+    {
+        bool disableCache = true;
+        SketchfabAPI.GetModel(_guid, (resp) =>
+        {
+            SketchfabModelImporter.Import(resp.Object, (obj) =>
+            {
+                if(obj != null)
+                {
+                    m_CameraController.PlaceObjectInFront(obj);
+                }
+            }, disableCache);
+        }, disableCache);
+    }
+}
